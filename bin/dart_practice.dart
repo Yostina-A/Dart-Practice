6507555c61e25a1b985d57b0bf30b18e0218a4c3
Yostin-A <yostina.aw@gmail.com>
2026-05-12 14:44:02 +0300

    Added an enum of hobby levels and made it display in capitalized case

--- bin/dart_practice.dart
+++ bin/dart_practice.dart
@@ -1,27 +1,44 @@
 
+enum HobbyLevel {
+  excelent, good, fair, weak;
+
+  String get label {
+    switch(this){
+    case HobbyLevel.excelent: return "Excelent";
+    case HobbyLevel.good: return "Good";
+    case HobbyLevel.fair: return "Fair";
+    case HobbyLevel.weak: return "Weak";
+    }
+  }
+}
 
 class Person {
   Person(this.name, this.age, this.hobbies);
   String name;
   int age;
-  Map<String,String> hobbies;
+  Map<String,HobbyLevel> hobbies;
 
   void printData(){
     print("Name: $name");
-    print("Age: $age");
-    print("Hobbies: $hobbies");
-  }
+    print("Age: $age \n");
+    
+    print("Hobies: ");
+    hobbies.forEach((key, value){
+      print('$key: ${value.label}');
+      }
+    );
+    }
 }
 
-class Hobbies {
+/*class Hobbies {
   Hobbies(this.name, this.category, this.tools);
 
   String name;
   String category;
   List<String> tools;
-}
+}*/
 
 void main(){
-  Person p1 = Person("Tina", 35, {"Drawing":"Good", "Cream Piping": "Good"});
+  Person p1 = Person("Tina", 35, {"Drawing": HobbyLevel.fair, "Cream Piping": HobbyLevel.good});
   p1.printData();
 }
