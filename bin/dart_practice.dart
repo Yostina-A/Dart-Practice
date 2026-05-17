0d43e4507dc46245ce992d1d3411ff501f69479d
Yostin-A <yostina.aw@gmail.com>
2026-05-17 16:10:57 +0300

    Practicing Inheritance

--- bin/dart_practice.dart
+++ bin/dart_practice.dart
@@ -1,9 +1,17 @@
 import 'oop_practice.dart';
 
 void main(){
-  Person p1 = Person("Tina", 35);
-  
-  // I used setters and getters here
-  print(p1.name);
-  print(p1.age);
+  //Engineer e1 = Engineer("Tina", 35 , "Software Devleoper");
+  //e1.work("an app");
+
+  //Doctor d1 = Doctor("Sam", 50, "Drawing");
+  //d1.work("Nadia");
+
+  //var car1 = Hyundai("Hyundai", 2008, "SUV", "Black");
+  //car1.go();
+  //car1.motorType = "1600 CC";
+  //car1.printMotorType();
+
+  var lucky = Dog("Mamal", "Lucky", moveType.walk, "Woof", "Border Collie");
+  lucky.move();
 }
--- bin/oop_practice.dart
+++ bin/oop_practice.dart
@@ -1,19 +1,103 @@
 class Person {
-  Person(this._name, this._age);
+  Person(this.name, this.age);
 
-  String _name;
-  int _age;
+  final String name;
+  final int age;
 
-  // the use of setters and getters to handle private properties.
-
-  set name (String name){
-    _name = name;
+  void walk(){
+    print("$name is walking");
   }
 
-  set age (int age){
-    _age = age;
+  void work(String value){
+    print("$name is working on $value");
+  }
+}
+
+
+class Engineer extends Person {
+  Engineer(super.name, super.age, this.job);
+
+  String job;
+
+  /*@override
+  void walk(){
+    super.walk();
+    print("$name has finished walking");
+  }*/
+  @override
+  void work(String value){
+    print("$name is developing $value");
+  }
+}
+
+class Doctor extends Person{
+  Doctor(super.name, super.age, this.hobby);
+
+  String hobby;
+
+  @override
+  void work(String value){
+    print("$name is a doctor and he is checking $value at the moment");
+  }
+}
+
+
+
+class Car {
+  Car(this.brand, this.model, this.type);
+
+  String brand;
+  int model;
+  String type;
+
+  void go(){
+    print("The $brand car is going");
+  }
+}
+
+class Hyundai extends Car{
+  Hyundai(super.brand, super.model, super.type, this.color);
+  
+  String color;
+  String? motorType;
+
+  void printMotorType(){
+    print("The $brand car model numer: $model has a motor type of $motorType");
   }
 
-  String get name => _name;
-  int get age => _age;
+}
+
+enum moveType {
+  fly, walk, swim;
+
+  String get label {
+    switch(this){
+      case moveType.fly: return "Fly";
+      case moveType.walk: return "Walk";
+      case moveType.swim: return "Swim";
+    }
+  }
+}
+class Animal {
+  Animal(this.type, this.name, this.moveHow, this.sound);
+
+  String type;
+  String name;
+  moveType moveHow;
+  String sound;
+
+  void move(){
+    print("$name is moving");
+  } 
+}
+
+class Dog extends Animal{
+  Dog(super.type, super.name, super.moveHow, super.sound, this.breed);
+
+  String breed;
+
+  @override
+  void move(){
+    print("$name is a dog of breed $breed and it says '$sound'");
+  }
 }
