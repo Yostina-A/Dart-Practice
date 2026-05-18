1f5eeeeaaa5da7c9c550d43f4d954231dc9dedde
Yostin-A <yostina.aw@gmail.com>
2026-05-18 16:51:44 +0300

    printed '$' using \$

--- bin/oop_practice.dart
+++ bin/oop_practice.dart
@@ -2,10 +2,11 @@
 
 
  abstract class Payment {
+
   void pay(double amount, String name); //this sysntax is called abstract method
 
   void printReciept(double amount){
-    print("You have paid the amount of $amount");
+    print("You have paid the amount of \$$amount"); // ading "\$ before $amount prints "$"
   }
 }
 
@@ -14,7 +15,7 @@ class PayByWallet extends Payment {
 
   @override
   void pay(double amount, String name) {
-    print("$name paid $amount using Wallet.");
+    print("$name paid \$$amount using Wallet.");
     //we don't need the "super" keyword here to call the parent class's method since we didn't
     //override it in the child class.
     printReciept(amount);
@@ -26,7 +27,7 @@ class PayByCreditCard extends Payment {
 
   @override
   void pay(double amount, String name) {
-    print("$name paid $amount using credit card.");
+    print("$name paid \$$amount using credit card.");
 
     printReciept(amount);
   }
